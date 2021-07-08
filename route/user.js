bea5f01b6abc5b071112374e1d1b5763dc4f9259
sahildabhi0101 <63159436+sahildabhi0101@users.noreply.github.com>
2021-07-08 09:45:40 +0530

    make it better

---
+++ route/user.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const {userbyid , getuser , updateuser }=require('../controller/user')
+const {requiresignin}=require('../controller/auth');
+
+
+const router = express.Router();
+
+router.get('/users/:userId',requiresignin, getuser);
+router.put('/users/:userId',requiresignin, updateuser);
+
+// any route containing :
+//userid ,our app will first execute userbyid()  
+router.param("userId",userbyid);
+
+module.exports =router;
+
